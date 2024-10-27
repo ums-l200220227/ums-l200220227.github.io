a0c941a066eb9c2252f907ab2e7e902370fb96aa
Pandu Adi Wijaya Kusuma Wardana <pappaunion@gmail.com>
2024-10-27 19:04:38 +0700

    some change

--- l200220227.py
+++ l200220227.py
@@ -1,19 +1,12 @@
 from metaflow import FlowSpec, step
 
-class kuliah_informatika(FlowSpec):
+class KuliahInformatika(FlowSpec):
     """Alur yang mensimulasikan perjalanan kuliah."""
-
-    def __init__(self):
-        super().__init__()
-        self.midterm_grade = None
-        self.final_grade = None
-        self.final_score = None
 
     @step
     def start(self):
         """Titik awal"""
-        print("Membayar biaya kuliah yang mencakup seluruh semester "
-              "untuk memastikan pengalaman kuliah yang lancar...")
+        print("Membayar biaya kuliah...")
         self.next(self.register)
 
     @step
@@ -25,35 +18,28 @@ class kuliah_informatika(FlowSpec):
     @step
     def attend_classes(self):
         """Langkah menghadiri kelas"""
-        print("Menghadiri kelas dan berpartisipasi dalam berbagai kegiatan...")
+        print("Menghadiri kelas...")
         self.total_attend = 14
-        self.presence = 0
-
-        for i in range(1, self.total_attend + 1):
-            attend = True  # Simulasi kehadiran
-            if attend:
-                self.presence += 1
-            print(f"Hari {i}: {'Hadir' if attend else 'Tidak hadir'}")
-        self.min_attend = int(0.75 * self.total_attend)
-        if self.presence >= self.min_attend:
+        self.presence = 14  # Simulasi kehadiran penuh
+        print(f"Kehadiran: {self.presence} dari {self.total_attend}")
+        if self.presence >= 0.75 * self.total_attend:
             print("Memenuhi persyaratan kehadiran")
+            self.next(self.receive_grades)
         else:
             print("Tidak memenuhi persyaratan kehadiran")
             self.next(self.end)
-        self.next(self.receive_grades)
 
     @step
     def receive_grades(self):
-        """Langkah menerima nilai UTS dan UAS"""
+        """Langkah menerima nilai"""
         self.midterm_grade = 80  # Nilai UTS
         self.final_grade = 90  # Nilai UAS
-        print(f"Nilai UTS: {self.midterm_grade}")
-        print(f"Nilai UAS: {self.final_grade}")
+        print(f"Nilai UTS: {self.midterm_grade}, Nilai UAS: {self.final_grade}")
         self.next(self.calculate_final_score)
 
     @step
     def calculate_final_score(self):
-        """Menghitung nilai akhir berdasarkan UTS dan UAS"""
+        """Menghitung nilai akhir"""
         self.final_score = (self.midterm_grade + self.final_grade) / 2
         print(f"Nilai akhir: {self.final_score}")
         self.next(self.end)
@@ -61,7 +47,7 @@ class kuliah_informatika(FlowSpec):
     @step
     def end(self):
         """Langkah akhir"""
-        print("Proses selesai dengan sukses!")
+        print("Proses selesai!")
 
 if __name__ == '__main__':
-    kuliah_informatika()
+    KuliahInformatika()
